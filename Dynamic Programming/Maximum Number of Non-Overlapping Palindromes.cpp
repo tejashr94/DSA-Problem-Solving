1414d7b641338fa230d80f64febbbf82169fa03d
Tejash Rai <tejashr94@gmail.com>
2026-09-15 12:02:13 +0530

    Add maximum non-overlapping palindromes DP solution

---
+++ Dynamic Programming/Maximum Number of Non-Overlapping Palindromes.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+
+        // pal[i][j] = true if s[i...j] is a palindrome
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+        // Build palindrome table
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+
+                if (s[i] == s[j] && (len <= 2 || pal[i + 1][j - 1])) {
+                    pal[i][j] = true;
+                }
+            }
+        }
+
+        // dp[i] = maximum number of non-overlapping palindromes
+        // using the first i characters
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            // Don't use character i-1
+            dp[i] = dp[i - 1];
+
+            // Try every substring ending at i-1
+            for (int j = 0; j < i; j++) {
+                if (i - j >= k && pal[j][i - 1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n^2)
