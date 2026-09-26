baaa6e69f9883c277a38fcb5bcafbc8a0609561b
Tejash Rai <tejashr94@gmail.com>
2026-09-26 22:21:28 +0530

    feat: add LeetCode 1807 initial bracket parsing

---
+++ LeetCode/1807-Evaluate-Bracket-Pairs/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        string ans;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '(') {
+                ans += s[i];
+                continue;
+            }
+
+            int j = i + 1;
+            while (s[j] != ')') j++;
+
+            string key = s.substr(i + 1, j - i - 1);
+            ans += key; // initial parsing version
+            i = j;
+        }
+        return ans;
+    }
+};
